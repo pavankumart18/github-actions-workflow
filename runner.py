89ba755b3856187922e01c79cfd6a1b9d6e59167
pavankumart18 <pavankumart7052@gmail.com>
2026-03-06 02:41:07 +0530

    updated workflow

--- runner.py
+++ runner.py
@@ -1,11 +1,14 @@
 import sys
 import subprocess
+import os
 
 task_file = sys.argv[1]
 
+event = os.getenv("GITHUB_EVENT_NAME")
+print("GitHub Event:", event)
+
 commands = []
 
-# read markdown file
 with open(task_file, "r") as f:
     for line in f:
         line = line.strip()
@@ -14,12 +17,11 @@ with open(task_file, "r") as f:
             cmd = line.replace("- run:", "").strip()
             commands.append(cmd)
 
-print("Commands found:")
+print("\nCommands found:")
 for c in commands:
     print(" ", c)
 
 print("\n--- Executing ---\n")
 
-# execute commands
 for cmd in commands:
     subprocess.run(cmd, shell=True)
